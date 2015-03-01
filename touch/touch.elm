18769d498ecc9fe2ba8e7a40377d4534f74d556f
cutsea110 <cutsea110@gmail.com>
2015-03-01 16:58:32 +0900

    [Refactor] update

--- touch/touch.elm
+++ touch/touch.elm
@@ -1,28 +1,24 @@
-import Color (..)
 import Graphics.Collage (..)
 import Graphics.Element (..)
 import List
 import Signal
-import Text (asText)
 import Time (Time, every)
 import Touch
 import Window
+
 import Temperature (kelvinToColor)
 
 main : Signal Element
-main = Signal.map3 scene Window.dimensions Touch.touches (every 1)
+main = Signal.map3 scene (every 1) Window.dimensions Touch.touches
 
-scene : (Int,Int) -> List Touch.Touch -> Time -> Element
-scene (w,h) touches t =
-    let dots = List.map (makeCircle t <| pair toFloat (w, h)) touches
-    in layers [ collage w h dots, asText t ]
+scene : Time -> (Int,Int) -> List Touch.Touch -> Element
+scene t (w,h) touches =
+    let marks = List.map (makeMark t <| (toFloat w, toFloat h)) touches
+    in layers [ collage w h marks ]
 
-pair : (a -> b) -> (a, a) -> (b, b)
-pair f (x, y) = (f x, f y)
-
-makeCircle : Time -> (Float, Float) -> Touch.Touch -> Form
-makeCircle now (w, h) {x, y, t0} =
-    let size = 50 + (now - t0) / 100
+makeMark : Time -> (Float, Float) -> Touch.Touch -> Form
+makeMark now (w, h) {x, y, t0} =
+    let size = 60 + (now - t0) / 100
         clr = kelvinToColor (now - t0) 0.8
     in
       ngon 5 size
